a3a016400328b84e2909a60d552b358ba518c307
Felicia WXP <felicia_wxp@aiap.sg>
2022-06-09 17:05:35 +0800

    update doc strings

--- src/aiap_team6_miniproject_fastapi/v1/routers/model.py
+++ src/aiap_team6_miniproject_fastapi/v1/routers/model.py
@@ -14,20 +14,16 @@ PRED_MODEL = team6_miniproject_fapi.deps.PRED_MODEL
 
 @ROUTER.post("/infer", status_code=fastapi.status.HTTP_200_OK)
 def predict_model(processed_file_path: str):
-    """Endpoint that returns sentiment classification of movie review
-    texts.
+    """Endpoint that returns dirty classification of floor image.
 
     Parameters
     ----------
-    movie_reviews_json : team6_miniproject_fapi.schemas.MovieReviews
-        'pydantic.BaseModel' object detailing the schema of the request
-        body
+    processed_file_path : str
 
     Returns
     -------
     dict
-        Dictionary containing the sentiments for each movie review in
-        the body of the request.
+        Dictionary containing the prediction for processed image of the request.
 
     Raises
     ------
@@ -50,53 +46,6 @@ def predict_model(processed_file_path: str):
     return {
         "data": {"prediction": dirt_prediction, "image_file_path": output_file_path}
     }
-
-
-@ROUTER.post("/predict", status_code=fastapi.status.HTTP_200_OK)
-def predict_sentiment(movie_reviews_json: team6_miniproject_fapi.schemas.MovieReviews):
-    """Endpoint that returns sentiment classification of movie review
-    texts.
-
-    Parameters
-    ----------
-    movie_reviews_json : team6_miniproject_fapi.schemas.MovieReviews
-        'pydantic.BaseModel' object detailing the schema of the request
-        body
-
-    Returns
-    -------
-    dict
-        Dictionary containing the sentiments for each movie review in
-        the body of the request.
-
-    Raises
-    ------
-    fastapi.HTTPException
-        A 500 status error is returned if the prediction steps
-        encounters any errors.
-    """
-    result_dict = {"data": []}
-
-    try:
-        logger.info("Generating sentiments for movie reviews.")
-
-        movie_reviews_dict = movie_reviews_json.dict()
-        review_texts_array = movie_reviews_dict["reviews"]
-        for review_val in review_texts_array:
-            curr_pred_result = PRED_MODEL.predict([review_val["text"]])
-            sentiment = "positive" if curr_pred_result > 0.5 else "negative"
-            result_dict["data"].append(
-                {"review_id": review_val["id"], "sentiment": sentiment}
-            )
-            logger.info(
-                "Sentiment generated for Review ID: {}".format(review_val["id"])
-            )
-
-    except Exception as error:
-        print(error)
-        raise fastapi.HTTPException(status_code=500, detail="Internal server error.")
-
-    return result_dict
 
 
 @ROUTER.get("/version", status_code=fastapi.status.HTTP_200_OK)
